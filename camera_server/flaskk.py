b250b5bbe629809d30678f52d5d34864f69c6ebb
Debasmit Roy <60804078+DEBASMITROY2002@users.noreply.github.com>
2024-05-27 11:42:21 +0530

    Update flaskk.py

--- camera_server/flaskk.py
+++ camera_server/flaskk.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, Response
-from camera import VideoCamera
+import cv2
 
+# Camera Servers to stream the video at http ports from videos from S3 bucket
+# The videos are stored in S3 bucket and the links are provided below
+
+# Camera wise video links
 video_list = {
     "0":0,
     "Cereals Copurchase":"https://rategain2023.s3.ap-south-1.amazonaws.com/videos/Cereals+Copurchase.mp4",
@@ -10,8 +14,6 @@ video_list = {
 }
 
 
-
-import cv2
 
 app = Flask(__name__)
 
@@ -32,18 +34,12 @@ def video_detection(path_x):
         # try:
             while cap.isOpened():
                 success, img = cap.read()
-                # 1.2 x zoom
-                # img = cv2.resize(img, (0,0), None, 1.2, 1.1)
                 if success == True:
                     print(img.shape)
                     print("SUCCESSS")
                     yield img
                 else:
                     cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
-            # cap = cv2.VideoCapture(path_x)
-        # except:
-        #     cap = cv2.VideoCapture(path_x)
-        #     print("BRUH")
 
 
 
@@ -53,53 +49,35 @@ def generate_frames(path_x = ''):
         ref,buffer=cv2.imencode('.jpg',detection_)
 
         frame=buffer.tobytes()
-        # yield (b'--frame\r\n'
-        #             b'Content-Type: image/jpeg\r\n\r\n' + frame +b'\r\n')
         yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-# @app.route('/v')
-# def video_feed():
-#     return Response(gen(VideoCamera(URL = "https://drive.google.com/uc?export=view&id=1DvjBD42U1ifiVvbeE3mi1y5kPwu_5O_K")),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# @app.route('/v2')
-# def video():
-#     return Response(generate_frames(path_x = "https://drive.google.com/uc?export=view&id=1mNqcLgeFwcJ_jMppnG9HMY9Xl9NAd-xz"),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-# @app.route('/v2')
-# def video():
-#     return Response(generate_frames(path_x = "https://cdn.discordapp.com/attachments/871472954486194206/1243467852854595595/Fish_theft.mp4?ex=6651952e&is=665043ae&hm=0164cd260e078f938114671ab761b004272a9ecbc6d9ae856f9a98580ae2fbae&"),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
+# This is the main function to stream the video from S3 bucket | Camera : Cereal Section
 @app.route('/v2')
 def video2():
     return Response(generate_frames(path_x = video_list["Cereals Copurchase"]),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
+# This is the main function to stream the video from S3 bucket | Camera : Fish Section
 @app.route('/v3')
 def video3():
     return Response(generate_frames(path_x = video_list["Fish theft"]),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+# This is the main function to stream the video from S3 bucket | Camera : Shaving Section
 @app.route('/v4')
 def video4():
     return Response(generate_frames(path_x = video_list["Shaving Copurchase"]),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+# This is the main function to stream the video from S3 bucket | Camera : Wine Section
 @app.route('/v5')
 def video5():
     return Response(generate_frames(path_x = video_list["Wine theft"]),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# @app.route('/v3')
-# def video2():
-#     return Response(gen(VideoCamera2(URL = "https://drive.google.com/uc?export=view&id=1DvjBD42U1ifiVvbeE3mi1y5kPwu_5O_K")),
-#                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
     app.run(port=8081,host='0.0.0.0', debug=True)
